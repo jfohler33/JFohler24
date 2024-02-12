e607c713a6472d4997a62b621cfa3d0868f88529
jfohler33 <jacobonval@gmail.com>
2024-02-12 11:24:32 -0800

    secrets

--- app/utils/secrets copy.py
+++ app/utils/secrets copy.py
@@ -2,11 +2,11 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':"",
-        'MONGO_DB_NAME':"",
+        'MONGO_HOST':"mongodb+srv://jfohler33:jfohler33@fohler33.wfo86iu.mongodb.net/JacobPractice?retryWrites=true&w=majority",
+        'MONGO_DB_NAME':"JacobPractice",
         'GOOGLE_CLIENT_ID': "",
         'GOOGLE_CLIENT_SECRET':"",
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
-        'MY_EMAIL_ADDRESS':""
+        'MY_EMAIL_ADDRESS':"s_jacob.fohler@ousd.org"
         }
     return secrets
