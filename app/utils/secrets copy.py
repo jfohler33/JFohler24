aa33a08ee5bdbb5f54ef3f665e966062ea9eae22
jfohler33 <jacobonval@gmail.com>
2024-02-14 11:33:24 -0800

    woop

--- app/utils/secrets copy.py
+++ app/utils/secrets copy.py
@@ -4,8 +4,8 @@ def getSecrets():
     secrets = {
         'MONGO_HOST':"mongodb+srv://jfohler33:jfohler33@fohler33.wfo86iu.mongodb.net/JacobPractice?retryWrites=true&w=majority",
         'MONGO_DB_NAME':"JacobPractice",
-        'GOOGLE_CLIENT_ID': "",
-        'GOOGLE_CLIENT_SECRET':"",
+        'GOOGLE_CLIENT_ID': "296103977700-j0pimk9ih2fjj8gl753ttu4chqp9r3co.apps.googleusercontent.com",
+        'GOOGLE_CLIENT_SECRET':"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",
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
         'MY_EMAIL_ADDRESS':"s_jacob.fohler@ousd.org"
         }
